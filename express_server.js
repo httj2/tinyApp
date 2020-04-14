7f67b85500648c17d73d635c5491d9216d60ef30
Judith Huynh <judithuynh@gmail.com>
2020-04-14 11:50:53 +0000

    changed the shortUrl req.parameter and changed port

--- express_server.js
+++ express_server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 3000; // default port 8080
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
+  "cp24cp24": "http://www.cp24.com",
   "9sm5xK": "http://www.google.com"
 };
 
@@ -16,9 +16,9 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase};
@@ -27,6 +27,6 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: "jtt3hs"};
+  let templateVars = { shortURL: (Object.keys(urlDatabase)[0]), longURL: urlDatabase.cp24cp24 };
   res.render("urls_show", templateVars);
 });
