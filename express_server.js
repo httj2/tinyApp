cdd73da40d332e2d4b4f5d9b95db6439216e69d1
Judith Huynh <judithuynh@gmail.com>
2020-04-14 13:49:30 +0000

    added parse and a new route urls/new

--- express_server.js
+++ express_server.js
@@ -8,14 +8,29 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+function generateRandomString() {
+
+}
+const bodyParser = require("body-parser");
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  if (res.statusCode !== 200) {
+    const msg = `Status Code ${res.statusCode} when fetching IP. Response: ${body}`;
+    callback(Error(msg), null);
+    return;
+  }
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 // app.get("/hello", (req, res) => {
 //   res.send("<html><body>Hello <b>World</b></body></html>\n");
 // });
@@ -24,6 +39,11 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase};
   // let templateVars = { greeting: 'Welcome!!' };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+  
 });
 
 app.get("/urls/:shortURL", (req, res) => {
