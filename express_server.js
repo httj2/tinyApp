f0050f70d099dc262bef3af9985dfa05bc023af8
Judith Huynh <judithuynh@gmail.com>
2020-04-16 22:14:00 +0000

    added a userID and updated the URLdatabase to take in UserID

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
-var cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 
 // const URLsRouter = require("./routes/urls");
 
@@ -17,49 +17,58 @@ app.use(bodyParser.urlencoded({extended: true}));
 //=============== DATABASES ==================//
 //======= url Database ===========
 const urlDatabase = {
-  "cp24cp24": "http://www.cp24.com",
-  "9sm5xK": "http://www.google.com"
+  "cp24cp24": {
+  longURL: "http://www.cp24.com",
+  userID: "aJ48lW"
+  },
+  "9sm5xK": {
+  longURL: "http://www.google.com",
+  userID: "aJ48lW"
+  }
 };
 // ===========user Database ============
-const usersDB = { 
+const usersDB = {
   "t93w12": {
-    id: "t93w12", 
-    email: "tw@gmail.com", 
+    id: "t93w12",
+    email: "tw@gmail.com",
     password: "sports"
   },
- "3t1ech": {
-    id: "3t1ech", 
-    email: "corgis@corgis.com", 
+  "3t1ech": {
+    id: "3t1ech",
+    email: "corgis@corgis.com",
     password: "chimken4ever"
   }
-}
+};
 //================ HELPER FUNCTIONS ====================//
 // ======= Add new URl ===========//
-const addNewURL = longURL => {
+const addNewURL = (longURL,userID) => {
   const shortURL = Math.random().toString(36).substr(2,8);
-  urlDatabase[shortURL] = longURL;
+  urlDatabase[shortURL] = {
+    longURL: longURL,
+    userID: userID
+  }
   return shortURL;
-}
+};
 
 const updateURL = (shortURL, longURL) => {
-  urlDatabase[shortURL] = longURL;
+  urlDatabase[shortURL].longURL = longURL;
 }
 // ========= Add new user with newID  ============//
 const addNewUser = (email, password) => {
   const userID = Math.random().toString(36).substr(2,8);
-  const newUser= {
-   id: userID, 
+  const newUser = {
+   id: userID,
    email: email,
    password: password
- };
- usersDB[userID] = newUser;
- return userID
+  };
+  usersDB[userID] = newUser;
+  return userID;
 };
 
 
 // ========== checkEmail ===========//
 checkEmailExists = (loginEmail) => {
-  for (id in usersDB) {
+  for (let id in usersDB) {
     if (usersDB[id].email === loginEmail) {
       return true;
     }
@@ -69,20 +78,20 @@ checkEmailExists = (loginEmail) => {
 // =======  Get user by email & password ======== ///
 const getUserByEmail = function(email) {
   for (let id in usersDB) {
-    if (usersDB[id].email === email){
+    if (usersDB[id].email === email) {
       return usersDB[id];
-    } 
-  }
+    }
+  };
   return false
 };
 
 
 // AuthenticateUser if its in the UsersDB.
 const authenticateUser = (email, password) => {
-  const user = getUserByEmail(email); //return user
+  const user = getUserByEmail(email);     //return user
   if (user && user.password === password) {
     return user;
-  } 
+  };
   return false;
 };
 
@@ -105,12 +114,12 @@ const authenticateUser = (email, password) => {
 app.get("/login", (req, res)=> {
   const templateVars = {
     currentUser: null
-  }
+  };
     res.render("user_login", templateVars)
 });
 // ========= Register Route =========//
 app.get("/register", (req, res) => {
-  const templateVars = { 
+  const templateVars = {
     currentUser: null,
   };
   res.render("user_registration", templateVars)
@@ -122,7 +131,7 @@ app.get("/urls", (req, res) => {
   //using user_ID look up the user of the usersDB;
   const userId = req.cookies['user_id'];
   const loggedUser = usersDB[userId];
-  const templateVars = { 
+  const templateVars = {
     urls: urlDatabase,
     currentUser: loggedUser,
   };
@@ -132,7 +141,7 @@ app.get("/urls", (req, res) => {
 
 // === Redirecting short URLs to long URLS ==============
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  const longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);
   //status code: 302 --> temporarily to a new page
 });
@@ -140,15 +149,18 @@ app.get("/u/:shortURL", (req, res) => {
 
 // ========= Submit new URL ============ //
 app.get("/urls/new", (req, res) => {
+  // check if user is logged in 
   const shortURL = req.params.shortURL;
   const userId = req.cookies['user_id'];
   const loggedUser = usersDB[userId];
-  let templateVars = { 
-    shortURL: shortURL, 
-    longURL: urlDatabase[shortURL],
-    currentUser: loggedUser
-  };
-  res.render("urls_new", templateVars);
+  if (!loggedUser) {
+    res.redirect('/login')
+  } else {
+    let templateVars = {
+      currentUser: loggedUser
+    };
+    res.render("urls_new", templateVars);
+  }  
 });
 
 // ======== show specific URL =========== //
@@ -158,7 +170,7 @@ app.get("/urls/:shortURL", (req, res) => {
   const loggedUser = usersDB[userId];
   let templateVars = { 
     shortURL: shortURL, 
-    longURL: urlDatabase[shortURL],
+    longURL: urlDatabase[shortURL].longURL,
     currentUser: loggedUser
   };
   res.render("urls_show", templateVars);
@@ -170,7 +182,8 @@ app.get("/urls/:shortURL", (req, res) => {
 //========= New URL to DB =================//
 app.post("/urls", (req, res) => {
   const longURL = req.body['longURL']; 
-  const shortURL = addNewURL(longURL);
+  const userID = req.cookies['user_id'];
+  const shortURL = addNewURL(longURL, userID);
   res.redirect(`/urls/${shortURL}`);
 });
 
